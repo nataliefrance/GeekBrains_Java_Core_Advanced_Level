f9da1b5bb361efa9f064b178f1589cc0e2f1f106
Natalia Shipova <nataliefrance@mail.ru>
2019-03-24 16:21:03 +0300

    Урок 7: код с урока

---
+++ src/Lesson6/client/Controller.java
@@ -0,0 +1,281 @@
+package Lesson6.client;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+//подписываем клиента на интерфейс Initializable для того,
+// чтобы при запуске он пытался подключиться к нашему серверу
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+
+
+public class Controller {
+    @FXML
+    TextArea textArea;
+
+    @FXML
+    TextField textField;
+
+    @FXML
+    Button btn1;
+
+    @FXML
+    HBox bottomPanel;
+
+    @FXML
+    HBox upperPanel;
+
+    @FXML
+    TextField loginField;
+
+    @FXML
+    PasswordField passwordField;
+
+    private boolean isAuthorized;
+
+    Socket socket;
+    DataInputStream in;
+    DataOutputStream out;
+
+    final String IP_ADRESS = "localhost";
+    final int PORT = 8189;
+
+    public void setAuthorized(boolean isAuthorized) {
+        this.isAuthorized = isAuthorized;
+        if(!isAuthorized) {
+            upperPanel.setVisible(true);
+            upperPanel.setManaged(true);
+            bottomPanel.setVisible(false);
+            bottomPanel.setManaged(false);
+        } else {
+            upperPanel.setVisible(false);
+            upperPanel.setManaged(false);
+            bottomPanel.setVisible(true);
+            bottomPanel.setManaged(true);
+        }
+    }
+
+    public void tryToAuth(ActionEvent actionEvent) {
+
+        if(socket == null || socket.isClosed()) {
+            connect();
+        }
+
+        try {
+            out.writeUTF("/auth " + loginField.getText() + " " + passwordField.getText());
+            loginField.clear();
+            passwordField.clear();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+    public void connect() {
+
+        try {
+            socket = new Socket(IP_ADRESS, PORT);
+            in = new DataInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
+
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        while (true) {
+                            String str = in.readUTF();
+                            if (str.startsWith("/authok")) {
+                                setAuthorized(true);
+                                break;
+                            } else {
+                                textArea.appendText(str + "\n");
+                            }
+                        }
+
+                        while (true) {
+                            String str = in.readUTF();
+                            if (str.equals("/serverclosed"))  {
+                                // closeApp();
+                                break;
+                            }
+                            textArea.appendText(str + "\n");
+                        }
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } finally {
+                        try {
+                            socket.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        setAuthorized(false);
+                    }
+                }
+            }).start();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendMsg() {
+        try {
+            out.writeUTF(textField.getText());
+            textField.clear();
+            textField.requestFocus();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+/*
+public class Controller {
+    @FXML
+    private
+    TextArea textArea;
+
+    @FXML
+    private
+    TextField textField;
+
+    @FXML
+    Button btn;
+
+    @FXML
+    HBox bottomPanel;
+
+    @FXML
+    HBox upperPanel;
+
+    @FXML
+    TextField loginField;
+
+    @FXML
+    PasswordField passwordField;
+
+    private boolean isAuthorized;
+
+    Socket socket;
+    DataInputStream in;
+    DataOutputStream out;
+
+    final String IP_ADRESS = "localhost";
+    final int PORT = 8189;
+
+    public void setAuthorized(boolean isAuthorized) {
+        this.isAuthorized = isAuthorized;
+        if(!isAuthorized) {
+            upperPanel.setVisible(true);
+            upperPanel.setManaged(true);
+            bottomPanel.setVisible(false);
+            bottomPanel.setManaged(false);
+        } else {
+            upperPanel.setVisible(false);
+            upperPanel.setManaged(false);
+            bottomPanel.setVisible(true);
+            bottomPanel.setManaged(true);
+        }
+    }
+
+    public void tryToAuth(ActionEvent actionEvent) {
+
+        if(socket == null || socket.isClosed()) {
+            connect();
+        }
+
+        try {
+            out.writeUTF("/auth " + loginField.getText() + " " + passwordField.getText());
+            loginField.clear();
+            passwordField.clear();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+    public void connect() {
+
+        try {
+            socket = new Socket(IP_ADRESS, PORT);
+            in = new DataInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
+
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        while (true) {
+                            String str = in.readUTF();
+                            if (str.startsWith("/authok")) {
+                                setAuthorized(true);
+                                break;
+                            } else {
+                                textArea.appendText(str + "\n");
+                            }
+                        }
+
+                        while (true) {
+                            String str = in.readUTF();
+                            if (str.equals("/serverclosed"))  {
+                                // closeApp();
+                                break;
+                            }
+                            textArea.appendText(str + "\n");
+                        }
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } finally {
+                        try {
+                            socket.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        setAuthorized(false);
+                    }
+                }
+            }).start();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendMsg() {
+        try {
+            out.writeUTF(textField.getText());
+            textField.clear();
+            textField.requestFocus();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+*/
---
+++ src/Lesson6/server/MainServer.java
@@ -0,0 +1,58 @@
+package Lesson6.server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.sql.SQLException;
+import java.util.Vector;
+
+public class MainServer {
+    private Vector<ClientHandler> clients;
+
+    public MainServer() throws SQLException {
+        ServerSocket server = null;
+        Socket socket = null;
+        clients = new Vector<>();
+
+        try {
+            AuthService.connect();
+            server = new ServerSocket(8189);
+            System.out.println("Сервер запущен!");
+
+            while (true) {
+                socket = server.accept();
+                System.out.println("Клиент подключился");
+                new ClientHandler(this,socket);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                server.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            AuthService.disconnect();
+        }
+    }
+
+    public void subscribe(ClientHandler client) {
+        clients.add(client);
+    }
+
+    public void unsubscribe(ClientHandler client) {
+        clients.remove(client);
+    }
+
+    public void broadcastMsg(String msg) {
+        for (ClientHandler o : clients) {
+            o.sendMsg(msg);
+        }
+    }
+}
